a51bdfe46cd887ad6b399840561ad326994b2cf5
chino702 <chinox3702@gmail.com>
2023-04-30 16:06:49 -0700

    palindrome solution 2

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  var reversed = word.split('').reverse().join('');
+  return word === reversed;
 }
 
 /* 
-  Add your pseudocode here
+1. Define the function isPalindrome that takes a string as input.
+2. Create a new string variable called reversedString and initialize it to an empty string.
+3. Use a loop to iterate over each character in the input string, starting from the last character and working backwards.
+4. In each iteration of the loop, add the current character to the reversedString variable.
+5. After the loop has finished, check if the reversedString variable is equal to the original input string.
+6. If the reversedString is equal to the input string, return true, otherwise return false.
 */
 
 /*
-  Add written explanation of your solution here
+ To fix the errors in the code, I changed the isPalindrome function so that it would return the correct value. I made sure to check if the reversed string was equal to the original string and returned true if it was, and false otherwise. Then, I ran the tests again, and everything passed!
 */
 
 // You can run `node index.js` to view these console logs
